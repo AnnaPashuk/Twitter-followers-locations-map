6925da98654fc64ff9c7b355173059409688d696
Anna Pashuk <54504935+AnnaPashuk@users.noreply.github.com>
2020-02-25 17:45:42 +0200

    Update hidden.py

--- hidden.py
+++ hidden.py
@@ -4,7 +4,7 @@
 # Create new App and get the four strings
 
 def oauth():
-    return {"consumer_key": "vrA7aEefVpMGSJEVBjLDlixX0",
-            "consumer_secret": "bEoA4LX5PYPK93fOO588cvfdiEpewDfCwuEwLBwOA03Xkjn4Fm",
-            "token_key": "1230433895990120450-92ziBaF3fpo316CxY3MpPBglVNs9gZ",
-            "token_secret": "wSOnPpzSPovLE4VVEonNWnsFKYRSiPHXGkiAfAvhYyRuI"}
+    return {"consumer_key": "v...0",
+            "consumer_secret": "b...m",
+            "token_key": "1...Z",
+            "token_secret": "w...I"}
